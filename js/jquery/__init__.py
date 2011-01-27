32a97ac6ee53a4d55348013130892af6530168ba
Jan-Jaap Driessen <devnull@localhost>
2011-01-27 11:22:48 +0100

    Add cdn info and minimal fanstatic version requirement.

--- js/jquery/__init__.py
+++ js/jquery/__init__.py
@@ -2,4 +2,16 @@ from fanstatic import Library, Resource
 
 library = Library('jquery', 'resources')
 
-jquery = Resource(library, 'jquery.js', minified='jquery.min.js')
+# CDN info from http://docs.jquery.com/Downloading_jQuery#CDN_Hosted_jQuery
+
+_jquery_minified = Resource(library, 'jquery.min.js', cdns={
+    'google': '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js',
+    'microsoft': '//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.min.js',
+    'jquery': '//code.jquery.com/jquery-1.4.4.min.js',
+})
+
+jquery = Resource(library, 'jquery.js', minified=_jquery_minified, cdns={
+    'google': '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js',
+    'microsoft': '//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.js',
+    'jquery': '//code.jquery.com/jquery-1.4.4.js',
+})
