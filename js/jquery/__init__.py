5ed7cdadc028ae084f9eb354ac5d41ed8fb542e2
Jan-Jaap Driessen <devnull@localhost>
2011-01-28 17:39:55 +0100

    Update CDN information. The jquery CDN does not support HTTPS.

--- js/jquery/__init__.py
+++ js/jquery/__init__.py
@@ -5,13 +5,17 @@ library = Library('jquery', 'resources')
 # CDN info from http://docs.jquery.com/Downloading_jQuery#CDN_Hosted_jQuery
 
 _jquery_minified = Resource(library, 'jquery.min.js', cdns={
-    'google': '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js',
-    'microsoft': '//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.min.js',
-    'jquery': '//code.jquery.com/jquery-1.4.4.min.js',
+    ('google', 'http'): 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js',
+    ('google', 'https'): 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js',
+    ('microsoft', 'http'): 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.min.js',
+    ('microsoft', 'https'): 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.min.js',
+    ('jquery', 'http'): 'http://code.jquery.com/jquery-1.4.4.min.js',
 })
 
 jquery = Resource(library, 'jquery.js', minified=_jquery_minified, cdns={
-    'google': '//ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js',
-    'microsoft': '//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.js',
-    'jquery': '//code.jquery.com/jquery-1.4.4.js',
+    ('google', 'http'): 'http://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js',
+    ('google', 'https'): 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js',
+    ('microsoft', 'http'): 'http://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.js',
+    ('microsoft', 'https'): 'https://ajax.aspnetcdn.com/ajax/jQuery/jquery-1.4.4.js',
+    ('jquery', 'http'): 'http://code.jquery.com/jquery-1.4.4.js',
 })
